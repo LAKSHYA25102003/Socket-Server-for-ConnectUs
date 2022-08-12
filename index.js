f3203e85c0877f3259f59f3e37d0cf960c466430
LAKSHYA <llakshya63@gmail.com>
2022-08-12 18:11:13 +0530

    debugginh the code

--- index.js
+++ index.js
@@ -4,44 +4,41 @@ const io = require("socket.io")(9000, {
     }
 })
 
-let users=[];
-
-const addUser = (userId, socketId,users) => {
-    if (userId !== null) {
-
-        !users.some((user) => user.userId === userId) && users.push({ userId, socketId });
-    }
+let users = [];
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) && users.push({ userId, socketId });
 }
 
-const removerUser = (socketId,users) => {
-    users = users.filter(user => user.socketId !== socketId);
+const removeUser = (sockId) => {
+    users = users.filter(user => user.socketId !== sockId);
 }
 
-const getUser=(userId,users)=>{
+const getUser = (userId) => {
     console.log(users);
-    return users.find(user=>user.userId===userId);
+    return users.find(user => user.userId === userId);
 }
 
 io.on("connection", (socket) => {
     console.log("user connected");
-    io.emit("welcome", "hii this is a socket server");
     socket.on("addUser", (userId) => {
-        addUser(userId, socket.id,users);
+        console.log(userId);
+        addUser(userId, socket.id);
         io.emit("getUsers", users);
     })
     // send the message
-    socket.on("sendMessage", ({senderId,recieverId,text})=>{
-        let reciever = getUser(recieverId,users);
+    socket.on("sendMessage", ({ senderId, recieverId, text }) => {
+        let reciever = getUser(recieverId);
         console.log(reciever);
-        if(reciever!==undefined)
-        {
-            io.to(reciever.socketId).emit("getMessage",{senderId,text});
+        console.log(recieverId);
+        if (reciever !== undefined) {
+            io.to(reciever.socketId).emit("getMessage", { senderId, text });
         }
     })
 
-    socket.on("disconnect", (socket) => {
+    socket.on("disconnect", () => {
         console.log("user is disconnected");
-        removerUser(socket.id,users);
+        removeUser(socket.id);
+        console.log(socket.id);
         io.emit("getUsers", users);
     })
 
